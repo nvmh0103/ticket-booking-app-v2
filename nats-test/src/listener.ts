10dde1586112e864bf5a8b0d184b202fa0d00c9f
nvmh.0103 <19520564@gm.uit.edu.vn>
2022-01-31 11:37:54 +0700

    continue testing

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats, { Message, Stan } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import  { TicketCreatedListener }from './events/ticket-created-listener'
 console.clear();
 
 const stan = nats.connect('ticket', randomBytes(4).toString('hex'),{
@@ -14,24 +15,10 @@ stan.on('connect', () => {
         process.exit();
     })
 
-    const subscription = stan.subscribe(
-        'ticket:created',
-        'orders-service-queue-group'
-    );
-    
-    const options = stan.subscriptionOptions()
-        .setManualAckMode(true);
-
-    subscription.on('message', (msg: Message) => {
-        const data = msg.getData();
-
-        if (typeof data === 'string'){
-            console.log(`Received event #${msg.getSequence()}, with data: ${data  }`)
-        }
-
-        msg.ack();
-    })
+    new TicketCreatedListener(stan).listen();
 })
 
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
+
+
